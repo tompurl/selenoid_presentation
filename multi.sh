7252d05c9aa77dd5b4db02230938055394fd8470
Tom Purl <tom.purl@wi-tronix.com>
2022-01-10 11:26:06 -0600

    Add central browser config and lorri stuff

--- multi.sh
+++ multi.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-HUB_URL='http://david.localdomain:4444/wd/hub'
+source ./browser_configs
 
-./simple-test.py 15 chrome 93.0 "${HUB_URL}" &
-./simple-test.py 10 firefox 92.0 "${HUB_URL}" &
-./simple-test.py 20 MicrosoftEdge 94.0 "${HUB_URL}" &
+USAGE="Usage: $0 http://hub_url:4444/wd/hub"
+
+die () {
+    >&2 echo "$1"
+    exit 1
+}
+
+# sanity check
+[ "$#" -ne 1 ] && die "$USAGE"
+
+HUB_URL="$1"
+
+./simple-test.py 15 "$BROWSER_ONE_NAME" "$BROWSER_ONE_VERSION_ONE"  "${HUB_URL}" &
+./simple-test.py 15 "$BROWSER_TWO_NAME" "$BROWSER_TWO_VERSION_ONE"  "${HUB_URL}" &
+./simple-test.py 15 "$BROWSER_THREE_NAME" "$BROWSER_THREE_VERSION_ONE"  "${HUB_URL}" &
--- multimulti.sh
+++ multimulti.sh
@@ -1,18 +1,32 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-./simple-test-selenoid.py 15 chrome 92.0 &
-./simple-test-selenoid.py 10 firefox 91.0 &
-./simple-test-selenoid.py 20 opera 78.0 &
-./simple-test-selenoid.py 15 chrome 92.0 &
-./simple-test-selenoid.py 10 firefox 91.0 &
-./simple-test-selenoid.py 20 chrome 93.0 &
-./simple-test-selenoid.py 15 chrome 92.0 &
-./simple-test-selenoid.py 10 opera 77.0 &
-./simple-test-selenoid.py 20 chrome 93.0 &
-./simple-test-selenoid.py 15 chrome 92.0 &
-./simple-test-selenoid.py 10 firefox 92.0 &
-./simple-test-selenoid.py 20 chrome 93.0 &
-./simple-test-selenoid.py 15 chrome 92.0 &
-./simple-test-selenoid.py 10 firefox 92.0 &
-./simple-test-selenoid.py 20 chrome 93.0 &
+source ./browser_configs
+
+USAGE="Usage: $0 http://hub_url:4444/wd/hub"
+
+die () {
+    >&2 echo "$1"
+    exit 1
+}
+
+# sanity check
+[ "$#" -ne 1 ] && die "$USAGE"
+
+HUB_URL="$1"
+
+./simple-test.py 15 "$BROWSER_ONE_NAME" "$BROWSER_ONE_VERSION_ONE" "$HUB_URL" &
+./simple-test.py 10 "$BROWSER_TWO_NAME" "$BROWSER_TWO_VERSION_ONE" "$HUB_URL" &
+./simple-test.py 20 "$BROWSER_THREE_NAME" "$BROWSER_THREE_VERSION_TWO" "$HUB_URL" &
+./simple-test.py 15 "$BROWSER_ONE_NAME" "$BROWSER_ONE_VERSION_ONE" "$HUB_URL" &
+./simple-test.py 10 "$BROWSER_TWO_NAME" "$BROWSER_TWO_VERSION_ONE" "$HUB_URL" &
+./simple-test.py 20 "$BROWSER_ONE_NAME" "$BROWSER_ONE_VERSION_TWO" "$HUB_URL" &
+./simple-test.py 15 "$BROWSER_ONE_NAME" "$BROWSER_ONE_VERSION_ONE" "$HUB_URL" &
+./simple-test.py 10 "$BROWSER_THREE_NAME" "$BROWSER_THREE_VERSION_ONE" "$HUB_URL" &
+./simple-test.py 20 "$BROWSER_ONE_NAME" "$BROWSER_ONE_VERSION_TWO" "$HUB_URL" &
+./simple-test.py 15 "$BROWSER_ONE_NAME" "$BROWSER_ONE_VERSION_ONE" "$HUB_URL" &
+./simple-test.py 10 "$BROWSER_TWO_NAME" "$BROWSER_TWO_VERSION_TWO" "$HUB_URL" &
+./simple-test.py 20 "$BROWSER_ONE_NAME" "$BROWSER_ONE_VERSION_TWO" "$HUB_URL" &
+./simple-test.py 15 "$BROWSER_ONE_NAME" "$BROWSER_ONE_VERSION_ONE" "$HUB_URL" &
+./simple-test.py 10 "$BROWSER_TWO_NAME" "$BROWSER_TWO_VERSION_TWO" "$HUB_URL" &
+./simple-test.py 20 "$BROWSER_ONE_NAME" "$BROWSER_ONE_VERSION_TWO" "$HUB_URL" &
